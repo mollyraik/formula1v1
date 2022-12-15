195415a8bf6c86210eb3a36377abcc990b1ea072
Molly Raik <molly.raik@gmail.com>
2022-12-14 20:48:48 -0500

    wip: fixes driver placement bug adds color coding

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const $year = $('#year')
 const $team = $('#team')
 
+const $heading = $('.parameters')
 const $driverA = $('#driverA')
 const $driverB = $('#driverB')
 
@@ -14,13 +15,16 @@ $("form").on("submit", handleGetData)
 
 function handleGetData(event) {
  
-    event.preventDefault()
+    event.preventDefault();
+    $('.rounds').remove();
+    $('#driver').text('');
   
   $.ajax({
-    url: `https://ergast.com/api/f1/${$year.val()}/constructors/${$team.val().replace(' ','_')}/results.json`
+    url: `https://ergast.com/api/f1/${$year.val()}/constructors/${$team.val().replace(' ','_')}/results.json?limit=50`
   }).then(
     (data) => {
        teamData = data;
+       renderHeading ()
        renderDrivers();
        renderRaceResults();
     },
@@ -42,13 +46,27 @@ function renderRaceResults() {
     //iterate through the races and create rows in the table for each race
     let races = teamData.MRData.RaceTable.Races
     for (let i = 0; i < races.length; i++){
-
-        let $newRow = $(`<tr>
-        <td>Round ${i+1}</td>
-        <td class=${i+1}>${races[i].Results[0].position}</td>
-        <td class=${i+1}>${races[i].Results[1].position}</td>
-        </tr>`)
-
-        $('tbody').append($newRow);
+        // debugger
+        if (`${races[i].Results[0].Driver.givenName} ${races[i].Results[0].Driver.familyName}` === $driverA.text()){
+            let $newRow = $(`<tr class='rounds'>
+                <td>Round ${i+1}</td>
+                <td>${races[i].raceName}</td>
+                <td class='green'>${races[i].Results[0].position}</td>
+                <td class='red'>${races[i].Results[1].position}</td>
+                </tr>`)
+            $('tbody').append($newRow);
+        } else {
+            let $newRow = $(`<tr class='rounds'>
+                <td>Round ${i+1}</td>
+                <td>${races[i].raceName}</td>
+                <td class='red'>${races[i].Results[1].position}</td>
+                <td class='green'>${races[i].Results[0].position}</td>
+                </tr>`)
+            $('tbody').append($newRow);
+        }
     }
+  }
+
+function renderHeading () {
+   $heading.text(`${teamData.MRData.RaceTable.Races[0].Results[0].Constructor.name}: ${teamData.MRData.RaceTable.season}`) 
   }
